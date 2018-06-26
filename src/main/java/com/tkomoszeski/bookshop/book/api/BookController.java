301654e5f47c3a8d0f75c11ecd5b1f1c29edc740
Tomasz Komoszesk <tomasz.komoszeski@gmail.com>
2018-06-26 21:59:40 +0200

    Adding configuration

--- src/main/java/com/tkomoszeski/bookshop/book/api/BookController.java
+++ src/main/java/com/tkomoszeski/bookshop/book/api/BookController.java
@@ -13,6 +13,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api/books")
 public class BookController{
             
-    @Autowired
-    IBookService bookService;
+   
 }

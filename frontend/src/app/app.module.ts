046c03764c4f3cedb1be1c68c176752d54c08b1d
Tomasz Komoszesk <tomasz.komoszeski@gmail.com>
2018-07-14 20:36:00 +0200

    Update angular version to 6.0 and installed ngx-bootstrap with scss styles

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,36 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
-import { HomeComponent } from './home/home.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { BooksComponent } from './books/books.component';
-import { AuthorsComponent } from './authors/authors.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { CartComponent } from './cart/cart.component';
-import { AdminComponent } from './admin/admin.component';
-
-
-
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    NavigationComponent,
-    BooksComponent,
-    AuthorsComponent,
-    LoginComponent,
-    RegisterComponent,
-    CartComponent,
-    AdminComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

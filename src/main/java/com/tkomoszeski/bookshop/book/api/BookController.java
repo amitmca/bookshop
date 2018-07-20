649c4d61b09335b778a1c559eda3cb19ba3c78df
Tomasz Komoszesk <tomasz.komoszeski@gmail.com>
2018-07-20 17:46:05 +0200

    fix

--- src/main/java/com/tkomoszeski/bookshop/book/api/BookController.java
+++ src/main/java/com/tkomoszeski/bookshop/book/api/BookController.java
@@ -11,5 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api/books")
 public class BookController{
             
+    @RequestMapping("hello")
+    public String hello()
+    {
+        return "Hello World! Rest Api is working!";
+    }
    
 }
